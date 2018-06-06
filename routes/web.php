872415d65b057ec4024cc174c9ae2f2e1bce21a6
Unknown <jeaeric@gmail.com>
2018-06-06 16:55:19 +0200

    authentification terminée
    ou presque
    /home

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,6 @@
 Route::get('/', 'UtilisateurController@form_connexion')->name('Accueil');
 
 Route::post('/connexion', 'UtilisateurController@connexion');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
