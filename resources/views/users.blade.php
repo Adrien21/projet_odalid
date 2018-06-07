65560e9eb542cace5ce35ab9489157da55282775
Mélodie <melodie.celimene@yahoo.fr>
2018-06-07 12:20:33 +0200

    test users

---
+++ resources/views/users.blade.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Utilisateur</title>
+	<meta charset="utf-8">
+</head>
+<body>
+
+	<table>
+		<thead>
+			<tr>
+				<th>Nom</th>
+				<th>Prenom</th>
+				<th>Sexe</th>
+				<th>Numero ID</th>
+				<th>Date De Validite</th>
+				<th>Type</th>
+				<th>Email</th>
+				<th>Date de Naissance</th>
+				<th>Numeor d'identite</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td>Lebec</td>
+				<td>Bastien</td>
+				<td>Homme</td>
+				<td>???</td>
+				<td>15/05/2020</td>
+				<td>???</td>
+				<td>lebec@live.fr</td>
+				<td>01/01/1980</td>
+				<td>???</td>
+			</tr>
+		</tbody>
+		<tfoot>
+			<tr>
+				<th>Nom</th>
+				<th>Prenom</th>
+				<th>Sexe</th>
+				<th>Numero ID</th>
+				<th>Date De Validite</th>
+				<th>Type</th>
+				<th>Email</th>
+				<th>Date de Naissance</th>
+				<th>Numeor d'identite</th>
+			</tr>
+		</tfoot>
+	</table>
+
+</body>
+</html>
