f674e09fefc374bc8bcb6e8c7258dcbda530a691
Unknown <valentin.pierron21@laposte.net>
2018-06-08 13:00:23 +0200

    Prototype page d'accueil

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,9 +3,9 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+        <div class="col-md-12" style="height: 75vh;">
+            <div class="card text-center">
+                <div class="card-header">Tableau de bord</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -15,8 +15,27 @@
                     @endif
 
                     Vous êtes connecté !
+                    <!-- (Derniers évènements ou laisser ce message ?) -->
+                </div>
+                <div class="list-group d-inline-flex flex-wrap flex-row justify-content-center text-center col-md-12 mt-3 dropright">
+                    <a href="#" class="list-group-item list-group-item-action list-group-item-primary w-25 py-5 m-2 text-center col-md-3">Utilisateurs</a>
+
+                    <a href="#" class="list-group-item list-group-item-action list-group-item-primary w-25 py-5 m-2 text-center col-md-3">Historique</a>
+
+                    <a href="#" class="dropdown-toggle list-group-item list-group-item-action list-group-item-primary w-25 py-5 m-2 text-center col-md-3" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" id="menu_infrastructure">Infrastructure</a>
+                    <div class="dropdown-menu" aria-labelledby="menu_infrastructure">
+                        <a href="#" class="dropdown-item">Zone</a>
+                        <a href="#" class="dropdown-item">Porte</a>
+                        <a href="#" class="dropdown-item">Relais</a>
+                        <a href="#" class="dropdown-item">Gâche</a>
+                        <a href="#" class="dropdown-item">Lecteur</a>
+                    </div>
+
+                    <a href="#" class="list-group-item list-group-item-action list-group-item-primary w-25 py-5 m-2 text-center col-md-3"></a>
+                    <a href="#" class="list-group-item list-group-item-action list-group-item-primary w-25 py-5 m-2 text-center col-md-3"></a>
                 </div>
             </div>
+            
         </div>
     </div>
 </div>
