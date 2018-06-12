5a1de2adcaca8ef2317ca08726657cf6e96f5462
Unknown <valentin.pierron21@laposte.net>
2018-06-12 15:44:22 +0200

    Redirection à l'authentification

--- app/Http/Controllers/LogPseudoController.php
+++ app/Http/Controllers/LogPseudoController.php
@@ -9,13 +9,13 @@ class LogPseudoController extends Controller
 {
     public function authentificate(Request $request){
 
-        $credentials = $request->only('name', 'password');
+        $credentials = $request->only('email', 'password');
 
         if(Auth::attempt($credentials)){
-            return redirect()->intended('index');
+            return redirect()->away('/');
         }
         else{
-            return redirect()->intended('index');
+            return redirect()->away('/login');
         }
     }
 }
