5fa0cbda51192cb7adb4091b8dc46ccf23e50a89
Unknown <jeaeric@gmail.com>
2018-06-13 10:42:03 +0200

    affichage de l'historique

--- resources/views/historiqueHome.blade.php
+++ resources/views/historiqueHome.blade.php
@@ -12,4 +12,28 @@
         echo "Prénom: " .$historique->prenom .", date: " .$historique->date;
       }
   -->
+  <table class="table table-striped">
+    <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Nom</th>
+      <th scope="col">Date Evenement</th>
+      <th scope="col">Porte</th>
+      <th scope="col">Etat</th>
+    </tr>
+    </thead>
+    <tbody>
+
+    @foreach ($historiques as $historique)
+      <tr>
+        <th scope='row'>{{ $historique->id }}</th>
+        <td>{{ $historique->identite_nom }}</td>
+        <td>{{ $historique->dateEvenement }}</td>
+        <td>{{ $historique->porte_nom }}</td>
+        <td>{{ $historique->etatEvenement }}</td>
+      </tr>
+    @endforeach
+    </tbody>
+  </table>
+  {{ $historiques->links() }}
 @endsection
