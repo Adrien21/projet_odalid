20ec9b6bad6dbbc66b52845c534e02ba8f7fd017
Unknown <jeaeric@gmail.com>
2018-06-13 11:37:19 +0200

    Auto stash before merge of "master" and "origin/master"

--- resources/views/utilisateursHome.blade.php
+++ resources/views/utilisateursHome.blade.php
@@ -4,5 +4,31 @@
 
 @section('content')
   <h1 class="text-center">Accueil utilisateurs</h1>
-    
+  <table class="table table-striped">
+    <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Compte</th>
+      <th scope="col">Email</th>
+      <th scope="col">Actif</th>
+      <th scope="col">Role</th>
+      <th scope="col">Date d'expiration</th>
+      <th scope="col">Dernière connexion</th>
+    </tr>
+    </thead>
+    <tbody>
+
+    @foreach ($users as $user)
+      <tr>
+        <th scope='row'>{{ $user->id }}</th>
+        <td>{{ $user->username }}</td>
+        <td>{{ $user->email }}</td>
+        <td>{{ $user->enabled }}</td>
+        <td>{{ $user->roles }}</td>
+        <td>{{ $user->expire_at }}</td>
+        <td>{{ $user->last_login }}</td>
+      </tr>
+    @endforeach
+    </tbody>
+  </table>
 @endsection
