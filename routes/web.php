fe78291df28e5197aa5cb7e553e1057dafb49c0f
Unknown <jeaeric@gmail.com>
2018-06-14 14:36:20 +0200

    controle par middleware AuthControlMiddleware
    Ajout du Middleware dans le Kernel.php ;
    maj des routes soumises à autorisation

--- routes/web.php
+++ routes/web.php
@@ -33,35 +33,35 @@ Route::post('/badges/create', 'CreateController@badges');
 Route::get('/badges/edit/{n?}', 'EditController@badges')->where('n', '[0-9]+')->name('BadgesEdit');
 Route::post('/badges/edit/{n?}', 'UpdateController@badges')->where('n', '[0-9]+')->name('BadgesUpdate');
 // route pour la suppression d'un badge dans la bdd
-Route::get('/badges/delete/{n?}', 'DeleteController@badges')->where('n', '[0-9]+')->name('BadgesDelete');
+Route::get('/badges/delete/{n?}', 'DeleteController@badges')->middleware('authControl')->where('n', '[0-9]+')->name('BadgesDelete');
 
 
 // Routes partie zones
-Route::get('/infrastructure/zones', 'HomeController@zones')->name('Zones');
+Route::get('/infrastructure/zones', 'HomeController@zones')->middleware('authControl')->name('Zones');
 // Edition zones
-Route::get('/infrastructure/zones/edit/{n?}', 'EditController@zones')->where('n', '[0-9]+')->name('ZonesEdit');
-Route::post('/infrastructure/zones/edit/{n?}', 'UpdateController@zones')->where('n', '[0-9]+')->name('ZonesUpdate');
+Route::get('/infrastructure/zones/edit/{n?}', 'EditController@zones')->middleware('authControl')->where('n', '[0-9]+')->name('ZonesEdit');
+Route::post('/infrastructure/zones/edit/{n?}', 'UpdateController@zones')->middleware('authControl')->where('n', '[0-9]+')->name('ZonesUpdate');
 
 // Routes partie portes
-Route::get('/infrastructure/portes', 'HomeController@portes')->name('Portes');
+Route::get('/infrastructure/portes', 'HomeController@portes')->middleware('authControl')->name('Portes');
 // Edition portes
-Route::get('/infrastructure/portes/edit/{n?}', 'EditController@portes')->where('n', '[0-9]+')->name('PortesEdit');
-Route::post('/infrastructure/portes/edit/{n?}', 'UpdateController@portes')->where('n', '[0-9]+')->name('PortesUpdate');
+Route::get('/infrastructure/portes/edit/{n?}', 'EditController@portes')->middleware('authControl')->where('n', '[0-9]+')->name('PortesEdit');
+Route::post('/infrastructure/portes/edit/{n?}', 'UpdateController@portes')->middleware('authControl')->where('n', '[0-9]+')->name('PortesUpdate');
 
 // Routes partie salles
-Route::get('/infrastructure/salles', 'HomeController@salles')->name('Salles');
+Route::get('/infrastructure/salles', 'HomeController@salles')->middleware('authControl')->name('Salles');
 // Edition salles
-Route::get('/infrastructure/salles/edit/{n?}', 'EditController@salles')->where('n', '[0-9]+')->name('SallesEdit');
-Route::post('/infrastructure/salles/edit/{n?}', 'UpdateController@salles')->where('n', '[0-9]+')->name('SallesUpdate');
+Route::get('/infrastructure/salles/edit/{n?}', 'EditController@salles')->middleware('authControl')->where('n', '[0-9]+')->name('SallesEdit');
+Route::post('/infrastructure/salles/edit/{n?}', 'UpdateController@salles')->middleware('authControl')->where('n', '[0-9]+')->name('SallesUpdate');
 
 // Routes partie gaches
-Route::get('/infrastructure/gaches', 'HomeController@gaches')->name('Gâches');
+Route::get('/infrastructure/gaches', 'HomeController@gaches')->middleware('authControl')->name('Gâches');
 // Edition gaches
-Route::get('/infrastructure/gaches/edit/{n?}', 'EditController@gaches')->where('n', '[0-9]+')->name('GâchesEdit');
-Route::post('/infrastructure/gaches/edit/{n?}', 'UpdateController@gaches')->where('n', '[0-9]+')->name('GâchesUpdate');
+Route::get('/infrastructure/gaches/edit/{n?}', 'EditController@gaches')->middleware('authControl')->where('n', '[0-9]+')->name('GâchesEdit');
+Route::post('/infrastructure/gaches/edit/{n?}', 'UpdateController@gaches')->middleware('authControl')->where('n', '[0-9]+')->name('GâchesUpdate');
 
 // Routes partie lecteurs
-Route::get('/infrastructure/lecteurs', 'HomeController@lecteurs')->name('Lecteurs');
+Route::get('/infrastructure/lecteurs', 'HomeController@lecteurs')->middleware('authControl')->name('Lecteurs');
 // Edition lecteurs
-Route::get('/infrastructure/lecteurs/edit/{n?}', 'EditController@lecteurs')->where('n', '[0-9]+')->name('LecteursEdit');
-Route::post('/infrastructure/lecteurs/edit/{n?}', 'UpdateController@lecteurs')->where('n', '[0-9]+')->name('LecteursUpdate');
+Route::get('/infrastructure/lecteurs/edit/{n?}', 'EditController@lecteurs')->middleware('authControl')->where('n', '[0-9]+')->name('LecteursEdit');
+Route::post('/infrastructure/lecteurs/edit/{n?}', 'UpdateController@lecteurs')->middleware('authControl')->where('n', '[0-9]+')->name('LecteursUpdate');
