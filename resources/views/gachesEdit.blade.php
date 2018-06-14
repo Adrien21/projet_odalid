f6d3dbe573809ccc8c087d30e013acbc5bf4c2c9
Unknown <valentin.pierron21@laposte.net>
2018-06-14 12:47:01 +0200

    correction

--- resources/views/gachesEdit.blade.php
+++ resources/views/gachesEdit.blade.php
@@ -8,8 +8,8 @@
 
 @section('type')
 	@if ($gache->type === "prd4")
-		<span class="ml-3">Type : PoE rail DIN 4 voies</span>
+		<span class="ml-3" name="nom">Type : PoE rail DIN 4 voies</span>
 	@else
-		<span class="ml-3">Type : PoE boitier 3 voies</span>
+		<span class="ml-3" name="nom">Type : PoE boitier 3 voies</span>
 	@endif
 @endsection
