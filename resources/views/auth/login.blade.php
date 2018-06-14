b9e1f9664bf864c12529a5cb50e8b9a6d1382e42
Unknown <leopoldassogba21@gmail.com>
2018-06-14 16:50:40 +0200

    Auto stash before merge of "master" and "origin/master"

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,69 +1,110 @@
 @extends('layouts.navigation')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
+
+<div class="view full-page-intro" style="background-image: url('img/fond1f.jpg'); background-repeat: no-repeat; background-size: cover; height: 100vh; ">
+
+    <!-- Mask & flexbox options-->
+    <div class="mask rgba-black-light d-flex justify-content-center align-items-center">
+
+      <!-- Content -->
+      <div class="container">
+
+        <!--Grid row-->
+        <div class="row wow fadeIn">
+
+          <!--Grid column-->
+          <div class="col-md-6 mb-4 white-text text-center text-md-left">
+
+            <h1 class="display-4 font-weight-bold"> Odalid </h1>
+
+            <hr class="hr-light">
+
+
+
+            <p class="mb-4 d-none d-md-block">
+              <strong>ODALID conçoit, fabrique et commercialise des équipements électroniques pour l'identification,
+                la sécurisation et la traçabilité. Spécialisée dans le développement de lecteurs/encodeurs sans contact,
+                 notre société propose des solutions RFID innovantes. Aujourd’hui grâce à notre avancée technologique,
+                  ODALID est le partenaire qui vous garantit une solution RFID adaptée.
+                Notre projet d’entreprise a été lauréat au « concours national de création d’entreprise innovantes 2008 » BPI,
+                 et est soutenu par l’incubateur régional de bourgogne PREMICE et le Conseil Régional de Bourgogne.
+                  ODALID est formé d’ingénieurs en électronique et informatique spécialisés dans la conception de systèmes embarqués.
+                   Notre métier : « vous faire profiter et vous doter des équipements d’identification les plus innovants basés sur la RFID »</strong>
+            </p>
+
+            <a target="_blank" href="http://odalid.com/fr" class="btn btn-primary btn-lg"> Notre Site Web
+              <i class="fa fa-globe" aria-hidden="true"></i>
+            </a>
+
+          </div>
+          <!--Grid column-->
+
+          <!--Grid column-->
+          <div class="col-md-6 col-xl-5 mb-4">
+
+            <!--Card-->
             <div class="card">
-                <div class="card-header">{{ __('Authentification') }}</div>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+              <!--Card content-->
+              <div class="card-body">
 
-                        <div class="form-group row">
-                            <label for="email" class="col-sm-4 col-form-label text-md-right">{{ __('Email') }}</label>
+                <!-- Form -->
+                <!-- Material form register -->
+                  <form method="POST" action="{{ route('login') }}">
+                     @csrf
+                      <p class="h4 text-center mb-4">Se connecter</p>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
+                      <!-- Material input email -->
+                      <div class="md-form">
+                          <i class="fa fa-envelope prefix grey-text"></i>
+                          <input type="email" id="materialFormRegisterEmailEx" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
+                          <label for="materialFormRegisterEmailEx">Email</label><br>
 
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
+                      </div>
 
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Mot de passe') }}</label>
+                      <!-- Material input password -->
+                      <div class="md-form">
+                          <i class="fa fa-lock prefix grey-text"></i>
+                          <input type="password" id="materialFormRegisterPasswordEx" class="form-control" name="password" required>
+                          <label for="materialFormRegisterPasswordEx">Mot de Passe</label><br>
+                      </div>
+                      <!-- Rectangular switch -->
+                      <label class="switch">
+                        <input type="checkbox">
+                        <span class="slider"> </span>
+                      </label><p class="text-right">Rester connecté</p>
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
+                      <div class="text-center mt-4">
+                          <button class="btn btn-primary" type="submit">Valider</button> <br><br>
+                          <a href="email.html"><p class="font-weight-bold text-right">Mot de passe oublié?</p></a>
+                      </div>
+                  </form>
+<!-- Material form register -->
+                <!-- Form -->
 
-                                @if ($errors->has('password'))
-                                    <span class="invalid-feedback">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
+              </div>
 
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> {{ __('Se souvenir de moi') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4 pl-0">
-                                <button type="submit" class="btn btn-primary ml-3">
-                                    {{ __('Se connecter') }}
-                                </button>
-
-                                <a class="btn btn-link pl-0" href="{{ route('password.request') }}">
-                                    {{ __('Mot de passe oublié ?') }}
-                                </a>
-                            </div>
-                        </div>
-                    </form>
-                </div>
             </div>
+            <!--/.Card-->
+
+          </div>
+          <!--Grid column-->
+
         </div>
+        <!--Grid row-->
+
+      </div>
+      <!-- Content -->
+
     </div>
-</div>
+    <!-- Mask & flexbox options-->
+
+
+
+      <!--/.Copyright-->
+
+
+  </div>
+
 @endsection
