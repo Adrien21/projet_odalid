5edd6aedd1dfc5f3997bdf29390c3586517a37f0
Unknown <valentin.pierron21@laposte.net>
2018-06-20 11:59:58 +0200

    Commentaires

--- app/Http/Controllers/UpdateController.php
+++ app/Http/Controllers/UpdateController.php
@@ -52,21 +52,27 @@ class UpdateController extends Controller
                                             ]);
         // ajoute automatiquement le user au groupe si type (referent) est renseigné
         $requete = Badge::find($n)->update(['groupe' => $groupe]);
+
         // Ajout autorisation
+        // Boucle sur le nombre de zone total
         for ($i = 1; $i <= (Zone::count()); $i++) {
+            // Variables nécessaires pour retrouver les champs du formulaire (nom de la variable => nom du champ dans la BDD = 'dateDebut' + "_" + numero de la zone que l'on boucle = $i)
             $datedebut = 'dateDebut_' .$i;
             $datefin = 'dateFin_' .$i;
             $ligne_bdd = DateExpiration::where('zone_id', $i)
                                         ->where('identite_id', $n);
-
+            // Si aucune entrée dans la table od_identitezone avec un zone_id = $i 
+            // ET que les variables datedebut et datefin ne sont pas null
+            // -> création d'une entrée dans od_identitezone
             if (!($ligne_bdd->exists()) && $req->$datedebut != null && $req->$datefin != null) {
                 $requete2 = DateExpiration::create(['dateDebut' => $req->$datedebut,
                                                     'dateFin' => $req->$datefin,
                                                     'identite_id' => $n,
                                                     'zone_id' => $i
                                                     ]);
-
+                // On récupère l'id de cette entrée créée
                 $lastid_identitezone = DB::getPdo()->lastInsertId();
+                // Et pour chaques jour de la semaine, une entrée est créée
                 for ($j = 0; $j <= 6; $j++) {
                     $requete3 = PlageHoraire::create(['heureDebut' => '00:00:00',
                                                       'heureFin' => '23:59:00',
@@ -74,23 +80,33 @@ class UpdateController extends Controller
                                                       'identiteZone_id' => $lastid_identitezone
                                                     ]);
                 }
+                // Sinon, si une entrée zone_id dans od_identitezone existe déjà
             } else {
+                // Mise à jour des dates de permissions avec od_identitezone.zone_id = $i (car $i = id de zone dans laquelle on boucle actuellement)
                 $requete2 = DateExpiration::where('zone_id', $i)
                                             ->where('identite_id', $n)
                                             ->update(['dateDebut' => $req->$datedebut,
                                                       'dateFin' => $req->$datefin
                                                     ]);
+                // On récupère l'entrée que l'on vient de modifier (pour avoir l'id)
                 $od_identitezone = DateExpiration::where('identite_id', $n)
                                                     ->where('zone_id', $i)
                                                     ->get();
                 $od_jour_maj = new Collection();
+                // On récupère tout les horaires et jours de permission associés à l'id od_identitezone que l'ont vient de mettre à jour
                 foreach ($od_identitezone as $id) {
                     $recup_jour = PlageHoraire::where('identiteZone_id', $id->id)->get();
                     $od_jour_maj = $od_jour_maj->merge($recup_jour);
                 }
+                // Pour chaque horaires récupérés, ont les mets à jour
                 foreach ($od_jour_maj as $update) {
+                    // Variables nécessaires pour la mise à jour
+                    // (nom de la variable => nom du champ dans la BDD = 'heureDebut' +
+                    // "_" + id de l'entrée dans od_identitezone que l'on vient de modifier +
+                    // "_" + nom de l'entrée dans od_jour (0 = dimanche, 1 = lundi, etc...))
                     $heuredebut = 'heureDebut_' .$update->identiteZone_id .'_' .$update->nom;
                     $heurefin = 'heureFin_' .$update->identiteZone_id .'_' .$update->nom;
+                    // Mise à jour selon l'identiteZone_id qui a 7 occurences, donc on affine avec le nom, qui est unique par zone
                     $requete3 = PlageHoraire::where('identiteZone_id', $update->identiteZone_id)
                                             ->where('nom', $update->nom)
                                             ->update(['heureDebut' => $req->$heuredebut,
